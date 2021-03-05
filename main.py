f4ef218eadb7b7101fe30016598ed06c3b6e1609
Alex McCune <alexmccune1224@gmail.com>
2021-03-05 01:04:42 -0800

    added revisions to the Live Streamer Check function as well as .gitignore file

--- main.py
+++ main.py
@@ -1,42 +1,64 @@
 import os
-import discord
+from re import U
+from urllib.parse import uses_fragment
+from discord import message
+from discord.utils import DISCORD_EPOCH
 from dotenv import load_dotenv
 import requests
-import json
+import datetime
 import twitch
+import discord
+from discord.ext import commands
+from discord import Embed
 
-load_dotenv()
-token = os.environ.get('DISCORD_TOKEN') 
 
 
-client = discord.Client()
 
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return(quote)
+client = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('$inspire'):
-        await message.channel.send(get_quote())
-
-    if message.content.startswith('$busybunni'):
-        await message.channel.send(twitch.get_twitch_profile_pic('busybunni'))
-
-    if message.content.startswith('$isonline'):
-        await message.channel.send(twitch.is_live('busybunni'))
+    print('Logged in as {0}'.format(client.user))
 
 
+@client.command()
+async def ping(ctx):
+    await ctx.send('Input received {0}ms'.format(round(client.latency)*1000))
+
+@client.command()
+async def streamer(ctx, streamer: str, debug = False):
+    live_status = twitch.get_live_status(streamer)
+    if live_status == True:
+        URL = "https://twitch.tv/{0}".format(streamer)
+        
+        streamerInfo = twitch.get_streamer_info(streamer)
+        embed = Embed(title=streamerInfo.stream_title, url = URL, colour=discord.Colour(0xf79772), timestamp=datetime.datetime.utcnow())
+        embed.set_image(url=streamerInfo.profile_pic)
+        embed.set_author(name=streamerInfo.username, url=URL, icon_url=streamerInfo.profile_pic)
+        embed.add_field(name="Game", value=twitch.get_game_name(streamerInfo.game_id), inline=True)
+
+        await ctx.send("Hey, {0} is **L I V E:**  <{1}>, go support them! ".format(streamer,URL))
+        await ctx.send(embed=embed)
+
+    if live_status == False:
+        pass
+
+@client.command()
+async def streamerjson(ctx, arg):
+    json_list = twitch.debug_stream(arg)
+    await ctx.send ("**Found {0} Streamers:**\n```{1}```".format(len(json_list), json_list))
+
+# @client.command()
+# async def embedded(ctx, arg):
+#     test = Embed(title=arg)
+#     await ctx.send(embed=test)
+
+
+
+
+
+#Run the Client
+load_dotenv()
 client.run(os.environ.get("DISCORD_TOKEN"))
+
+
