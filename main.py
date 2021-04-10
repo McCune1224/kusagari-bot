ec245dfee642a26193b61be630af20fd3e31434d
Alex McCune <alexmccune1224@gmail.com>
2021-04-10 09:51:54 -0700

    idek just pushing to work on other laptop

--- main.py
+++ main.py
@@ -1,64 +1,86 @@
+import requests
 import os
+import twitch
 from re import U
 from urllib.parse import uses_fragment
+import discord
+from discord.voice_client import VoiceClient
 from discord import message
 from discord.utils import DISCORD_EPOCH
 from dotenv import load_dotenv
-import requests
 import datetime
-import twitch
-import discord
 from discord.ext import commands
 from discord import Embed
 
 
-
-
 client = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
-    print('Logged in as {0}'.format(client.user))
+    print(f"Logged in as {client.user}")
 
 
 @client.command()
 async def ping(ctx):
-    await ctx.send('Input received {0}ms'.format(round(client.latency)*1000))
+    await ctx.send(f"Input received {round(client.latency)*1000}ms")
+
 
 @client.command()
-async def streamer(ctx, streamer: str, debug = False):
+async def streamer(ctx, streamer: str, debug=False):
     live_status = twitch.get_live_status(streamer)
     if live_status == True:
-        URL = "https://twitch.tv/{0}".format(streamer)
-        
-        streamerInfo = twitch.get_streamer_info(streamer)
-        embed = Embed(title=streamerInfo.stream_title, url = URL, colour=discord.Colour(0xf79772), timestamp=datetime.datetime.utcnow())
-        embed.set_image(url=streamerInfo.profile_pic)
-        embed.set_author(name=streamerInfo.username, url=URL, icon_url=streamerInfo.profile_pic)
-        embed.add_field(name="Game", value=twitch.get_game_name(streamerInfo.game_id), inline=True)
+        URL = f"https://twitch.tv/{streamer}"
 
-        await ctx.send("Hey, {0} is **L I V E:**  <{1}>, go support them! ".format(streamer,URL))
+        streamerInfo = twitch.get_streamer_info(streamer)
+        embed = Embed(title=streamerInfo.stream_title, url=URL, colour=discord.Colour(
+            0xf79772), timestamp=datetime.datetime.utcnow())
+        embed.set_image(url=streamerInfo.profile_pic)
+        embed.set_author(name=streamerInfo.username, url=URL,
+                         icon_url=streamerInfo.profile_pic)
+        embed.add_field(name="Game", value=twitch.get_game_name(
+            streamerInfo.game_id), inline=True)
+
+        await ctx.send(f"Hey, {streamer} is **L I V E:**  <{URL}>, go support them! ")
         await ctx.send(embed=embed)
 
     if live_status == False:
         pass
 
+
 @client.command()
 async def streamerjson(ctx, arg):
-    json_list = twitch.debug_stream(arg)
-    await ctx.send ("**Found {0} Streamers:**\n```{1}```".format(len(json_list), json_list))
-
+    try:
+        json_list = twitch._debug_stream(arg)
+        await ctx.send(f"**Found {len(json_list)} Streamers:**\n```{json_list}```")
+    except:
+        await ctx.send(f"Unable to find streamer {arg}")
 # @client.command()
 # async def embedded(ctx, arg):
 #     test = Embed(title=arg)
 #     await ctx.send(embed=test)
 
 
+@client.command()
+async def play(ctx, arg: str):
+    try:
+        author = ctx.message.author
+        print(author)
+        channel = author.voice.channel
+        print(channel)
+        await channel.connect() 
+    except:
+        print(f"Unable to join channel {channel.connect().cr_code}")
+# @bot.command()
+# async def join(ctx):
+#     channel = ctx.author.voice.channel
+#     await channel.connect()
+# @bot.command()
+# async def leave(ctx):
+#     await ctx.voice_client.disconnect()
 
 
 
-#Run the Client
+# Run the Client
 load_dotenv()
 client.run(os.environ.get("DISCORD_TOKEN"))
-
-
