7247810f40d49c2887fbc3537815be97c14c9192
Alex McCune <alexmccune1224@gmail.com>
2021-04-10 18:14:26 -0700

    added GIPHY search feature for bot

---
+++ giphy.py
@@ -0,0 +1,27 @@
+import os
+import random
+from dotenv import load_dotenv
+import json
+import requests
+import sys
+import giphy_client
+from giphy_client.rest import ApiException
+
+
+load_dotenv()
+GIPHY_TOKEN = os.environ.get('GIPHY_KEY')
+
+giphy_api = giphy_client.DefaultApi()
+
+'''helper function to search for'''
+def _search_gif(query):
+    try:
+        return giphy_api.gifs_search_get(GIPHY_TOKEN, query, limit=20)
+    except ApiException as e:
+        return f"Exception was called when searching for {query}:\n{e}" 
+
+def send_gif(phrase):
+    gifs = _search_gif(phrase)
+    response = random.choices(list(gifs.data))
+    return response[0].url
+
