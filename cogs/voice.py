d50f9afe2186e80efe05353f67171b1270b6ea2f
Alex McCune <alexmccune1224@gmail.com>
2021-06-30 17:39:47 -0700

    pushing code to test ffmpeg

---
+++ cogs/voice.py
@@ -0,0 +1,64 @@
+import os
+import time
+import discord
+from discord import client
+from discord.ext import commands
+from discord import message
+from discord.voice_client import VoiceClient
+import giphyAPI
+
+
+
+import os
+import discord
+from discord.utils import DISCORD_EPOCH
+from discord.ext import commands
+from dotenv import load_dotenv
+import nacl
+
+
+class VoiceChat(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.droidDir = os.path.join(os.getcwd(), "droidSounds")
+
+    @commands.command(pass_context=True)
+    async def join(self, ctx):
+        author = ctx.message.author
+        channel = author.voice.channel
+        if channel:
+            await channel.connect()
+
+    @commands.command()
+    async def leave(self, ctx):
+        if ctx.voice_client:
+            await ctx.guild.voice_client.disconnect()
+
+    @commands.command()
+    async def gif(self, ctx, *args):
+        arg_phrase = " ".join(args[:])
+        try:
+            await ctx.send(giphyAPI.send_gif(arg_phrase))
+        except:
+            print(f"Unable to send GIF of {arg_phrase}")
+            await ctx.send(f"No GIF of {arg_phrase} exists...")
+
+    @commands.command()
+    async def roger(self, ctx, phrase="roger"):
+        author = ctx.message.author
+        voice_channel = author.voice.channel
+        if voice_channel != None:
+            vc = await voice_channel.connect()
+            vc.play(discord.FFmpegPCMAudio(
+                source=os.path.join(self.droidDir, phrase+".mp3")))
+            # Sleep while audio is playing.
+            while vc.is_playing():
+                time.sleep(.1)
+            await vc.disconnect()
+        else:
+            await ctx.send(str(ctx.author.name) + "is not in a channel.")
+        # await ctx.message.delete()
+
+
+def setup(client):
+    client.add_cog(VoiceChat(client))
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import requests
-import time 
 import os
-import sys
-import twitch
-from re import U
-from urllib.parse import uses_fragment
 import discord
 from discord.voice_client import VoiceClient
-from discord import message
 from discord.utils import DISCORD_EPOCH
 from discord.ext import commands
-from discord import Embed
 from dotenv import load_dotenv
-import datetime
-import giphy
 import nacl
 
 
 client = commands.Bot(command_prefix='%')
 
+@client.command()
+async def load(ctx, extention):
+    client.load_extension(f"cogs.{extention}")
+
+@client.command()
+async def unload(ctx, extention):
+    client.unload_extension(f"cogs.{extention}")
+
+@client.command()
+async def reload(ctx, extention):
+    client.unload_extension(f"cogs.{extention}")
+    client.load_extension(f"cogs.{extention}")
 
 @client.event
 async def on_ready():
@@ -31,75 +33,15 @@ async def ping(ctx):
     await ctx.send(f"Input received {round(client.latency)*1000}ms")
 
 
-@client.command()
-async def streamer(ctx, streamer: str):
-    live_status = twitch.get_live_status(streamer)
-    if live_status == True:
-        URL = f"https://twitch.tv/{streamer}"
-
-        streamerInfo = twitch.get_streamer_info(streamer)
-        embed = Embed(title=streamerInfo.stream_title, url=URL, colour=discord.Colour(
-            0xf79772), timestamp=datetime.datetime.utcnow())
-        embed.set_image(url=streamerInfo.profile_pic)
-        embed.set_author(name=streamerInfo.username, url=URL,
-                         icon_url=streamerInfo.profile_pic)
-        embed.add_field(name="Game", value=twitch.get_game_name(
-            streamerInfo.game_id), inline=True)
-
-        await ctx.send(f"Hey, {streamer} is **L I V E:**  <{URL}>, go support them! ")
-        await ctx.send(embed=embed)
-
-    if live_status == False:
-        pass
 
 
-@client.command()
-async def streamerjson(ctx, arg):
-    try:
-        json_list = twitch._debug_stream(arg)
-        await ctx.send(f"**Found {len(json_list)} Streamers:**\n```{json_list}```")
-    except:
-        await ctx.send(f"Unable to find streamer {arg}")
-
-@client.command(pass_context=True)
-async def join(ctx):
-    author = ctx.message.author
-    channel = author.voice.channel
-    if channel:
-        await channel.connect()
 
 
-@client.command()
-async def leave(ctx):
-    if ctx.voice_client:
-        await ctx.guild.voice_client.disconnect()
-@client.command()
-async def gif(ctx,*args):
-    arg_phrase = " ".join(args[:])
-    try:
-        await ctx.send(giphy.send_gif(arg_phrase))
-    except:
-        print(f"Unable to send GIF of {arg_phrase}")
-        await ctx.send(f"No GIF of {arg_phrase} exists...")
-
-
-droidDir = os.path.join(os.getcwd(), "droidSounds")
-@client.command()
-async def roger(ctx, phrase="roger"):
-        author = ctx.message.author
-        voice_channel = author.voice.channel
-        if voice_channel != None:
-            vc = await voice_channel.connect()
-            vc.play(discord.FFmpegPCMAudio(source=os.path.join(droidDir, phrase+".mp3")))
-            # Sleep while audio is playing.
-            while vc.is_playing():
-                time.sleep(.1)
-            await vc.disconnect()
-        else:
-            await ctx.send(str(ctx.author.name) + "is not in a channel.")
-        # await ctx.message.delete()
-
+#Read for available Cogs
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f"cogs.{filename[:-3]}")
 
 # Run the Client
 load_dotenv()
-client.run(os.environ.get("DISCORD_TOKEN"))/home/alexm/Projects/kusagari-bot/droidSounds/roger.mp3
+client.run(os.environ.get("DISCORD_TOKEN"))
